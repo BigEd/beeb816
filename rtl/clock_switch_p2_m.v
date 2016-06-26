a4da57adeec4cfa98b0865fc1dae4524ce5e1d41
revaldinho <richevans@mac.com>
2016-06-26 18:15:05 +0100

    removed unused optional code from switch

--- rtl/clock_switch_p2_m.v
+++ rtl/clock_switch_p2_m.v
@@ -26,65 +26,44 @@ module clock_switch_p2_m (
    reg hs_enable_q,
        ls_enable_q;
    
-   reg selected_ls_q;
+   reg selected_ls_q,
+       selected_hs_q;   
 
    wire retimed_hs_enable_w;
    wire retimed_ls_enable_w;   
    
 
-`ifdef CLOCK_SWITCH_USE_LATCH_D
-   reg selected_hs_q;
-`endif
-   
-
    wire ck_w = (hs_ck_ip & hs_enable_q ) | (ls_ck_ip & ls_enable_q);   
    assign ck_op = ck_w;
 
-
-   assign selected_ls_op =  selected_ls_q;    
+   assign selected_ls_op = selected_ls_q;
+   assign selected_hs_op = selected_hs_q;
+   
    // The 'status' returned needs to be edge triggered for use in a feedback loop
    always @ (posedge ls_ck_ip or negedge resetb) 
      if ( ! resetb )
        selected_ls_q <= 1'b0;
      else
        selected_ls_q <= !select_hs_ip & !retimed_hs_enable_w;
-   
-   
-   // Create the clock enabling state with latches or flops as required
-`ifdef CLOCK_SWITCH_USE_LATCH_D
-   assign selected_hs_op = selected_hs_q;
-   
-   always @ (hs_ck_ip or resetb or select_hs_ip or retimed_ls_enable_w )
-     if ( ! resetb )
-       hs_enable_q <= 1'b0;   
-     else if ( hs_ck_ip == 1'b0 )
-       hs_enable_q <= select_hs_ip & !retimed_ls_enable_w;
 
-   // The 'status' returned needs to be edge triggered for use in a feedback loop
    always @ (posedge hs_ck_ip or negedge resetb) 
      if ( ! resetb )
        selected_hs_q <= 1'b0;
      else
-       selected_hs_q <= hs_enable_q & !retimed_ls_enable_w;
-   
-`else 
-   assign selected_hs_op = hs_enable_q;
+       selected_hs_q <= select_hs_ip & !retimed_ls_enable_w;
 
-   always @ (negedge hs_ck_ip or negedge resetb )
+   always @ ( negedge hs_ck_ip or negedge resetb )
      if ( ! resetb )
        hs_enable_q <= 1'b0;   
-     else
+     else 
        hs_enable_q <= select_hs_ip & !retimed_ls_enable_w;
 
-`endif // !`ifdef CLOCK_SWITCH_USE_LATCH_D
-   
-   always @ (negedge ls_ck_ip or negedge resetb )
+   always @ ( negedge ls_ck_ip or negedge resetb )
      if ( ! resetb )
        ls_enable_q <= 1'b1;   
-     else
+     else 
        ls_enable_q <= !select_hs_ip & !retimed_hs_enable_w;
 
-   // Pipe depth needs to be at least 3
    `define PIPE_SZ 2
    reg [`PIPE_SZ-1:0]  pipe_retime_ls_enable_q;
    reg [`PIPE_SZ-1:0]  pipe_retime_hs_enable_q;
@@ -94,7 +73,7 @@ module clock_switch_p2_m (
    
    always @ ( negedge  hs_ck_ip or posedge ls_enable_q or negedge resetb ) 
      if ( ! resetb )
-       pipe_retime_ls_enable_q <= {`PIPE_SZ{1'b0}};
+       pipe_retime_ls_enable_q <= {`PIPE_SZ{1'b1}};
      else if ( ls_enable_q )
        pipe_retime_ls_enable_q <= {`PIPE_SZ{1'b1}};
      else
