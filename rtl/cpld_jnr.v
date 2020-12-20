5d9c3ff305f8e430e7aca0bdf33a76cfaa23917c
revaldinho <richevans@mac.com>
2020-12-20 11:25:48 +0000

    White space & swap of some wires to defines update

--- rtl/cpld_jnr.v
+++ rtl/cpld_jnr.v
@@ -5,6 +5,11 @@
 `define PAGED_ROM_SEL 16'hFE30
 `define BPLUS_SHADOW_RAM_SEL 16'hFE34
 
+// Decode jumpers on J[1:0]
+`define BEEB_MODE   (j==2'b00)
+`define BPLUS_MODE  (j==2'b01)
+`define ELK_MODE    (j==2'b10)
+`define MASTER_MODE (j==2'b11)
 module cpld_jnr (
                  input [15:0]  cpu_adr,
                  input [1:0]   j,
@@ -15,22 +20,11 @@ module cpld_jnr (
                  output [11:0] bbc_adr
 		 );
 
-  wire                         elk_mode_w;
-  wire                         beeb_mode_w;
-  wire                         bplus_mode_w;
-  wire                         master_mode_w;
-
   reg [11:0]                   bbc_adr_lat_q;
 
-  // Decode jumpers on J[1:0]
-  assign beeb_mode_w = (j==2'b00);
-  assign bplus_mode_w = (j==2'b01);
-  assign elk_mode_w = (j==2'b10);
-  assign master_mode_w = (j==2'b11);
-
   assign bbc_adr = bbc_adr_lat_q;
-  assign dec_shadow_reg = (bplus_mode_w) ? (cpu_adr==`BPLUS_SHADOW_RAM_SEL) : 1'b0;
-  assign dec_rom_reg = (elk_mode_w)? (cpu_adr==`ELK_PAGED_ROM_SEL) : (cpu_adr==`PAGED_ROM_SEL);
+  assign dec_shadow_reg = (`BPLUS_MODE) ? (cpu_adr==`BPLUS_SHADOW_RAM_SEL) : 1'b0;
+  assign dec_rom_reg = (`ELK_MODE)? (cpu_adr==`ELK_PAGED_ROM_SEL) : (cpu_adr==`PAGED_ROM_SEL);
 
   // Flag FE4x (VIA) accesses and also all &FC, &FD expansion pages 
   assign dec_fe4x = (cpu_adr[15:4]==12'hFE4) || (cpu_adr[15:9]==7'b1111_110);
@@ -38,6 +32,5 @@ module cpld_jnr (
   always @ ( * )
     if ( lat_en  )
       bbc_adr_lat_q <= cpu_adr[11:0];
-
 
 endmodule
